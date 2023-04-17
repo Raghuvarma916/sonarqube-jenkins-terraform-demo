50036cff44777c366a97c1da5917c584bf4b6560
Raghuvarma916 <110372596+Raghuvarma916@users.noreply.github.com>
2023-04-17 16:02:44 +0530

    ami has changed

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                         = var.image_id     #"ami-09d56f8956ab235b3"
+  ami                         = var.image_id     #"ami-00b3e95ade0a05b9b"
   instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
   count                       = var.inst_count
   availability_zone           = "us-east-1c"
