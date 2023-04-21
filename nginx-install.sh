1239fbe682c2ad0c0495341c55ee33b4700c1919
raghuvarma <raghuvarma.com>
2023-04-21 11:37:04 +0000

    nginx file updated

--- nginx-install.sh
+++ nginx-install.sh
@@ -5,3 +5,4 @@ sudo systemctl enable nginx
 sudo systemctl start nginx
 echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /usr/share/nginx/html/index.html
 echo "<h1>Nginx was deployed successful using terraform $(hostname -f)</h1>" > /var/www/html/index.html
+echo "<h1>Successfully deployed Nginx <h1>
